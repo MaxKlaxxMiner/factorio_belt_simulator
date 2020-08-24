2677bc64181ee683fc67abd013ae7ea088d9b05a
MaxKlaxx <max@tetrisk.de>
2020-08-24 22:51:02 +0200

    v0.001: split tools.ts / game.ts

--- FactorioTransportBeltSimulator/ts/game.js
+++ FactorioTransportBeltSimulator/ts/game.js
@@ -1,13 +1,4 @@
 /* tslint:disable:one-line max-line-length interface-name comment-format no-bitwise */
-var requestAnimFrame = (function () { return (window.requestAnimationFrame || window.webkitRequestAnimationFrame || window.mozRequestAnimationFrame || (function (cb) { return window.setTimeout(cb, 1000 / 60); })); })();
-function getDocumentSize() {
-    var body = document.body;
-    var html = document.documentElement;
-    return {
-        width: Math.max(body.scrollWidth, body.offsetWidth, html.clientWidth, html.scrollWidth, html.offsetWidth),
-        height: Math.max(body.scrollHeight, body.offsetHeight, html.clientHeight, html.scrollHeight, html.offsetHeight)
-    };
-}
 var Game = /** @class */ (function () {
     function Game(gameDiv, canvasWidth, canvasHeight) {
         this.title = document.title;
